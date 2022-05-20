5c80c11c41c9c61b66f5f0c4329089ae4013f09e
koraycengiz <mkceng48@gmail.com>
2022-05-20 14:55:22 +0300

    first trial

---
+++ src/Armor.java
@@ -0,0 +1,11 @@
+public class Armor extends Item{
+
+    final double weightofLeatherArmor = 0.2;
+    final double weightofSteelArmor = 0.5;
+    final double weightofPlatinumArmor = 1;
+
+    //Protection reduces the taken damage.
+    final int protectionofLeatherArmor = 1;
+    final int protectionpfSteelArmor = 2;
+    final int protectionofPlatinumArmor = 5;
+}
