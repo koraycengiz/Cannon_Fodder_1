d1604ac9ec3ef9d9d149a665eb1a32729b2e8c11
SpaceD0G3 <105922892+SpaceD0G3@users.noreply.github.com>
2022-05-30 23:55:54 +0300

    Add files via upload

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,6 +1,7 @@
 public class Enemy extends Character{
 
     private int enemyHP;
+    private boolean isStunned;
 
     public int getEnemyHP(){
         enemyHP = (2*strength)+(7*vitality)+intelligence;
@@ -23,5 +24,10 @@ public class Enemy extends Character{
     public int getIntelligence() {
         intelligence = random.nextInt(5)+1;
         return intelligence;
+    }
+
+    public void setStunStatus(){// will work on this later
+        System.out.println("target succesfully stunned");
+        isStunned = true;
     }
 }
--- src/Fighter.java
+++ src/Fighter.java
@@ -12,4 +12,29 @@ public class Fighter extends Character {
         strength = random.nextInt(5)+6;
         return strength;
     }
+
+
+  //start of inventory related codes
+    public void calculateInventorySpace(){
+        super.calculateInventorySpace();
+    }
+    public void displayInventory(){
+        super.displayInventory();
+    }
+    public void pickItem(){
+        super.pickItem();
+    }
+    public void selectItem(){
+        super.selectItem();
+    }
+    public void examineItem(){
+        super.examineItem();
+    }
+    public void equipItem(){
+        super.equipItem();
+    }
+    public void discardItem(){
+        super.discardItem();
+    }
+
 }
