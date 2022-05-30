0563e9fd9467e89609df4b5171c324e29d0a15b4
iumut <iumutcakici@gmail.com>
2022-05-31 00:03:19 +0300

    early stage outline

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
         Character myCharacter = new Character();
         Fighter myFighter = new Fighter();
@@ -12,15 +15,31 @@ public class Test {
         Wand myWand = new Wand();
         Armor myArmor = new Armor();
 
-        //HPLERİ YANLIŞ HESAPLIYOR DÜZELT!!
 
-        System.out.println("Welcome to the game CANNON FODDER!");
-        System.out.println("-----------------------------------------------------------------------------------------------");
+        boolean offSwitch=true;
 
-
-        System.out.println("Fighter created with S:"+mySword.myStrength+" V:"+myFighter.getVitality()+" I:"+myFighter.getIntelligence()+".The HP is "+myFighter.getFighterHP()+".Fighter wields Katana with "+mySword.calculateDamageofKatana()+" damages and "+mySword.weightofKatana+" units of weight.");
-        System.out.println("Tank created with S:"+myTank.getStrength()+" V:"+myShield.myVitality+" I:"+myTank.getIntelligence()+".The HP is "+myTank.getTankHP()+".Tank wields RoundShield with "+myShield.calculateDamageofRoundShield()+" damages and "+myShield.weightofRoundShield+" unit of weight.");
-        System.out.println("Healer created with S:"+myHealer.getStrength()+" V:"+myHealer.getVitality()+" I:"+myWand.myIntelligence+".The HP is "+myHealer.getHealerHP()+".Healer wields GoldenWand with "+myWand.calculateDamageofGoldenWand()+" damages and "+myWand.weightofGoldenWand+" unit of weight.");
+        while (offSwitch){
+            System.out.println("""
+                    Welcome to the game CANNON FODDER!
+                    -----------------------------------------------------------------------------------------------
+                    Press 1 to start the game
+                    Press 2 to view high scores
+                    Press 3 to terminate the game"""
+            );
+            int usersMenuChoice = input.nextInt();
+            switch (usersMenuChoice){
+                case 1:
+                    System.out.println("Fighter created with S:"+mySword.myStrength+" V:"+myFighter.getVitality()+" I:"+myFighter.getIntelligence()+".The HP is "+myFighter.getFighterHP()+".Fighter wields Katana with "+mySword.calculateDamageofKatana()+" damages and "+mySword.weightofKatana+" units of weight.");
+                    System.out.println("Tank created with S:"+myTank.getStrength()+" V:"+myShield.myVitality+" I:"+myTank.getIntelligence()+".The HP is "+myTank.getTankHP()+".Tank wields RoundShield with "+myShield.calculateDamageofRoundShield()+" damages and "+myShield.weightofRoundShield+" unit of weight.");
+                    System.out.println("Healer created with S:"+myHealer.getStrength()+" V:"+myHealer.getVitality()+" I:"+myWand.myIntelligence+".The HP is "+myHealer.getHealerHP()+".Healer wields GoldenWand with "+myWand.calculateDamageofGoldenWand()+" damages and "+myWand.weightofGoldenWand+" unit of weight.");
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    offSwitch = false;
+                    break;
+            }
+        }
 
     }
 }
